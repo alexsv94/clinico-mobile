83ef429010096e2edf975638a330b18ea1ac46d3
alexsv94 <alex_savchuk94@mail.ru>
2022-02-08 21:15:25 +0300

    main

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import { useTheme } from './hooks/useTheme';
-import DeseasesPage from './pages/DeseasesPage';
-import FavoritesPage from './pages/FavoritesPage';
-import MainPage from './pages/MainPage';
-import MedicationsPage from './pages/MedicationsPage';
-import ProfilePage from './pages/ProfilePage';
+// import DeseasesPage from './pages/DeseasesPage';
+// import FavoritesPage from './pages/FavoritesPage';
+// import MainPage from './pages/MainPage';
+// import MedicationsPage from './pages/MedicationsPage';
+// import ProfilePage from './pages/ProfilePage';
+
+const DeseasesPage = lazy(() => import('./pages/DeseasesPage'));
+const FavoritesPage = lazy(() => import('./pages/FavoritesPage'));
+const MainPage = lazy(() => import('./pages/MainPage'));
+const MedicationsPage = lazy(() => import('./pages/MedicationsPage'));
+const ProfilePage = lazy(() => import('./pages/ProfilePage'));
 
 function App() {
 	const { theme, setTheme } = useTheme(localStorage.getItem('colorTheme') || 'light')
-	
+
 	return (
 		<BrowserRouter>
-			<NavBar theme={theme} setTheme={setTheme} />
-			<Routes>
-				<Route path='/' element={<MainPage />}/>
-				<Route path='/profile' element={<ProfilePage />}/>
-				<Route path='/deseases' element={<DeseasesPage />}/>
-				<Route path='/medications' element={<MedicationsPage />}/>
-				<Route path='/favorites' element={<FavoritesPage />}/>
-			</Routes>
+			<Suspense fallback={<div>Загрузка...</div>}>
+				<NavBar theme={theme} setTheme={setTheme} />
+				<Routes>
+					<Route path='/' element={<MainPage />} />
+					<Route path='/profile' element={<ProfilePage />} />
+					<Route path='/deseases' element={<DeseasesPage />} />
+					<Route path='/medications' element={<MedicationsPage />} />
+					<Route path='/favorites' element={<FavoritesPage />} />
+				</Routes>
+			</Suspense>
 		</BrowserRouter>
 	);
 }
